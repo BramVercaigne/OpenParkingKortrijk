f93bae731f59878e6fe97d1f2d0f3a4112830412
BramVercaigne <BramVercaigne@Brams-MacBook-Pro.local>
2013-10-21 11:12:27 +0200

    reset

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+init();
+
+function init(){
+	var btn = document.getElementById('saveButton');
+	btn.addEventListener('click', validate);
+};
+
+function validate(){
+	var voornaamOK = checkRequiredField('voornaam');
+	var naamOK = checkRequiredField('naam');
+	var emailOK = checkEmail('email');
+	
+	var voornaam = document.getElementById('voornaam');
+	if (!voornaamOK){		
+		voornaam.className = "error";	
+	}else{
+		voornaam.className = "";	
+	};
+	
+};
+
+function checkRequiredField(fieldId){
+	var control = document.getElementById(fieldId);
+	
+	if (!control){
+		return false	
+	}else{
+		return true	
+	};
+};
+//
+//function checkEmail(fieldId){
+//	return document.getElementById(fieldID).value.indexOf('@') > 0;
+//	
+//}
