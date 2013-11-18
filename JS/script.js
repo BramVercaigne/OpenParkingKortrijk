c9716ecba5315764de20ae5b668e5752ec9d3bc0
BramVercaigne <BramVercaigne@Brams-MacBook-Pro.local>
2013-11-18 12:23:51 +0100

    Optimalisation

--- JS/script.js
+++ JS/script.js
@@ -22,7 +22,8 @@ function deg2rad(deg) {
 	dataType: 'json',
 	success: function (data){
 		var html = "";
-		for (var i=0;i<data.bezettingparkings.parking.length;i++){
+		var lengthData = data.bezettingparkings.parking.length;
+		for (var i=0;i<lengthData;i++){
 			//alert(data.bezettingparkings.parking[i].parking);
 			var status = "";
 			var free = data.bezettingparkings.parking[i]._vrij;
@@ -45,27 +46,54 @@ function deg2rad(deg) {
 			}
 
 
-			html += 
-				"<div class='panel panel-"+status+"'>"+
-					"<div class='panel-heading'>"+
-						"<h3 class='panel-title'>"+
-							data.bezettingparkings.parking[i].parking+
-							"<span id='km-" + data.bezettingparkings.parking[i].parking + "' class='badge pull-right'>"+
+			// html += 
+			// 	"<div class='panel panel-"+status+"'>"+
+			// 		"<div class='panel-heading'>"+
+			// 			"<h3 class='panel-title'>"+
+			// 				data.bezettingparkings.parking[i].parking+
+			// 				"<span id='km-" + data.bezettingparkings.parking[i].parking + "' class='badge pull-right'>"+
 								
-							"</span>"+ 
-							"<span class='badge pull-right'>"+
-								data.bezettingparkings.parking[i]._vrij+
-							"</span>"+
-						"</h3>"+
-					"</div>"+
-					"<div class='panel-body'>"+
-						"<p>" +
-							data.bezettingparkings.parking[i]._vrij+"/"+data.bezettingparkings.parking[i]._capaciteit+
-						"</p>"+
-					"</div>"+
-				"</div>"
-			;
-			
+			// 				"</span>"+ 
+			// 				"<span class='badge pull-right'>"+
+			// 					data.bezettingparkings.parking[i]._vrij+
+			// 				"</span>"+
+			// 			"</h3>"+
+			// 		"</div>"+
+			// 		"<div class='panel-body'>"+
+			// 			"<p>" +
+			// 				data.bezettingparkings.parking[i]._vrij+"/"+data.bezettingparkings.parking[i]._capaciteit+
+			// 			"</p>"+
+			// 		"</div>"+
+			// 	"</div>"
+			// ;
+
+			html += "<div class='panel panel-";
+			html += status;
+			html += "'>";
+			html += "<div class='panel-heading'>";
+			html += "<h3 class='panel-title'>";
+			html += data.bezettingparkings.parking[i].parking;
+			html += "<span id='km-";
+			html += data.bezettingparkings.parking[i].parking;
+			html += "' class='badge pull-right'>";
+			html += "</span>";
+			html += "<span class='badge pull-right'>";
+			html += data.bezettingparkings.parking[i]._vrij;
+			html += "</span>";
+			html += "</h3>";
+			html += "</div>";
+			html += "<div class='panel-body'>";
+			html += "<p>";
+			html += data.bezettingparkings.parking[i]._vrij;
+			html += "/";
+			html += data.bezettingparkings.parking[i]._capaciteit;
+			html += "</p>";
+			html += "</div>";
+			html += "</div>";
+
+
+
+
 		}
 		document.getElementById("parking").innerHTML = html;
 		getLocation();
@@ -87,10 +115,12 @@ function showPosition(position)
 	dataType: 'json',
 	success: function (info){
 		
-		for (var i=0;i<info.parkoinfo.Authority.Operator.OffstreetParking.length;i++){
+		var lengthInfo = info.parkoinfo.Authority.Operator.OffstreetParking.length;
+		for (var i=0;i<lengthInfo;i++){
 			var km = getDistanceFromLatLonInKm(position.coords.latitude, position.coords.longitude, info.parkoinfo.Authority.Operator.OffstreetParking[i].GeneralInfo.GeoLocation.Latitude, info.parkoinfo.Authority.Operator.OffstreetParking[i].GeneralInfo.GeoLocation.Longitude);
 			km = Math.round(km * 100) / 100;
-			document.getElementById("km-"+info.parkoinfo.Authority.Operator.OffstreetParking[i].GeneralInfo.IDInfo.Name).innerHTML = "<span class='glyphicon glyphicon-road'></span>" + " " +km + " km / sp: " + position.coords.speed;
+			var speed = Math.round(position.coords.speed * 100) / 100;
+			document.getElementById("km-"+info.parkoinfo.Authority.Operator.OffstreetParking[i].GeneralInfo.IDInfo.Name).innerHTML = "<span class='glyphicon glyphicon-road'></span>" + " " +km + " km / sp: " + speed;
 			
 			//alert(position.coords.latitude+","+ position.coords.longitude+"&"+latparking+","+longparking);
 		}
